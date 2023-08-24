36ccc01832f84201533b35717606f2e3655dc2c8
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-08-24 22:06:40 +0300

    fix class grid

--- class.js
+++ class.js
@@ -15,7 +15,7 @@ export class Grid {
         this.cells = [];
         for (let i = 0; i < cellsCount; i++) {
             this.cells.push(
-                new Cell(gridElement, i % gridSize, Math.floor(i % gridSize))
+                new Cell(gridElement, i % gridSize, Math.floor(i / gridSize))
             );
             
         }
