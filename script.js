44c7d64c5f4e6cff14987d5739762b9e18d2106d
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-08-24 23:27:14 +0300

    update tilejs

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ import { Tile } from "./tile.js";
 const gameField = document.getElementById("field");
 const grid = new Grid(gameField);
 grid.getRandomEmptyCell().linkTile(new Tile(gameField));
+grid.getRandomEmptyCell().linkTile(new Tile(gameField));
