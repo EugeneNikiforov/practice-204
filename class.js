53e718a378fb158a912c64db16fb87c33d8b3770
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-08-24 22:26:06 +0300

    add celljs

--- class.js
+++ class.js
@@ -1,15 +1,7 @@
+import { Cell } from "./cell.js";
+
 const gridSize = 4;
 const cellsCount = gridSize * gridSize;
-class Cell {
-    constructor(gridElement, x, y) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        gridElement.append(cell);
-        this.x = x;
-        this.y = y;
-    }
-}
-
 export class Grid {
     constructor(gridElement) {
         this.cells = [];
